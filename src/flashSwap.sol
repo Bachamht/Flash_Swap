1f262fb86a59610c0d90673c3ecb49a799698afb
agito <1634263395@qq.com>
2024-03-05 11:06:06 +0800

    Flash loan success

--- src/flashSwap.sol
+++ src/flashSwap.sol
@@ -24,7 +24,7 @@ contract FlashSwap {
         CGAAddress = _CGAAddress;
         CGBAddress = _CGBAddress;
         router1 = _router1;
-        router2 = _router1;
+        router2 = _router2;
     }
     
     function flashLoan(uint CGATokenAmount) external {
@@ -42,16 +42,21 @@ contract FlashSwap {
         //Make sure it's the right pair to call the contract
         address token0 = IUniswapV2Pair(msg.sender).token0(); 
         address token1 = IUniswapV2Pair(msg.sender).token1(); 
-        assert(msg.sender == IUniswapV2Factory(factory1).getPair(token0, token1)); 
+        assert(msg.sender == IUniswapV2Factory(factory1).getPair(token0, token1));
+
+        //Swap out tokens from another trading pair
         (uint256 lendAmounut) = abi.decode(data, (uint256));
         address [] memory path = new address [](2);
         path[0] = CGAAddress;
         path[1] = CGBAddress;
         uint256 CGAAmount = CGAToken(CGAAddress).balanceOf(address(this));
         CGAToken(CGAAddress).approve(address(router2), CGAAmount);
-        UniswapV2Router02(payable(router2)).swapExactTokensForTokens(CGAAmount, 2000, path, address(this), block.timestamp + 60);
+        UniswapV2Router02(payable(router2)).swapExactTokensForTokens(CGAAmount, 3000, path, address(this), block.timestamp + 60);
+
+        //Calculate the amount due and make repayments
+        address pair1 = IUniswapV2Factory(factory1).getPair(CGAAddress, CGBAddress);
         (uint reserveA, uint reserveB) = UniswapV2Library.getReserves(factory1, CGAAddress, CGBAddress);
-        uint amountRepayment = UniswapV2Router02(payable(router2)).getAmountIn(lendAmounut, reserveA, reserveB);
-        CGBToken(CGBAddress).transfer(router1, amountRepayment);
+        uint amountRepayment = UniswapV2Router02(payable(router1)).getAmountIn(100, 10000, 500);
+        CGBToken(CGBAddress).transfer(pair1, amountRepayment);
     }
 }
